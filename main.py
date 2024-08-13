2d7933429fdf1ee6832e71ce682151370de108ee
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-13 13:16:11 +0100

    edit progress and add favicon

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from fasthtml.common import *
 
 tlink = Script(src="https://cdn.tailwindcss.com")
 
+favicon = Link(rel="icon", type="image/x-icon", href="/public/favicon.ico")
+
 def render(site):
     sid= f'site-{site.ROWID}'
     delete = A("Delete", hx_delete=f"/{site.ROWID}",hx_swap="outerHTML", target_id=f'{sid}')
     name_link = A(site.name, href=site.url, target="_blank")
     return Tr(Td(name_link), Td(site.tech), Td(site.service), Td(site.status), Td(delete), id=sid)
 
-app, rt, sites, Site= fast_app("mysites.db", live=True, ROWID=int, name=str, tech=str, service=str, status=str, pk='ROWID', url=str, render=render, hdrs=(tlink,picolink))
+app, rt, sites, Site= fast_app("mysites.db", live=True, ROWID=int, name=str, tech=str, service=str, status=str, pk='ROWID', url=str, render=render, hdrs=(tlink,picolink, favicon))
 
 def add_inputs(): return Input(placeholder="add new site", id="name", hx_swap_oob="true"), Input(placeholder="tech", id="tech", hx_swap_oob="true"), Input(placeholder="service", id="service", hx_swap_oob="true"), Input(placeholder="url", id="url", hx_swap_oob="true")
 
@@ -20,17 +22,18 @@ def get():
             title="Deployment Dashboard",
             site_name="Deployment Dashboard",
             description="A tool to check Jack's Deployments",
-            image="https://vercel.fyi/fasthtml-og",
+            image="public/favicon.ico",
             url="https://fasthtml-template.vercel.app",
         ),
+
     frm = Form (Group(add_inputs(), Button("Add") ),
-        hx_post ="/", target_id="sites-table", hx_swap="beforeend", cls="flex flex-col gap-2",
+        hx_post ="/", target_id="sites-table", hx_swap="beforeend",
     )
     table = Table(
     Thead(Tr(Th("Name"), Th("Tech"), Th("Service"), Th("Status"), Th("Edit"))),
     Tbody(*sites(), id="sites-table"),
 )
-    return Titled(H1("Deployment Dashboard", cls="text-4xl font-bold text-white-600 py-6"), Card(table, header=frm, footer=Footer()))
+    return  Title('Deployment Dashboard'), Card(H1("Deployment Dashboard", cls="text-4xl font-bold text-white-600 py-6"), Card(table, header=frm, footer=Footer()))
 
 def Footer():
     return (
